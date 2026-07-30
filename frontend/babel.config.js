3eae7e607b8aa19fc1def91daa545f2aeafb1d34
violetmoorway-9942 <violetmoorway@gmail.com>
2026-07-30 07:35:22 +0100

    fix: use babel module-resolver to mock agora-rtc-sdk-ng on Android/iOS builds to avoid Hermes crashes, rather than deleting it from package.json

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,6 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ['module-resolver', {
+        alias: {
+          'agora-rtc-sdk-ng': process.env.EXPO_PUBLIC_WEB_BUILD === 'true' 
+            ? 'agora-rtc-sdk-ng' 
+            : './src/mocks/agora-rtc-sdk-ng.mock.js'
+        }
+      }]
+    ]
   };
 };
---
+++ frontend/src/mocks/agora-rtc-sdk-ng.mock.js
@@ -0,0 +1,5 @@
+// Mock file for agora-rtc-sdk-ng to prevent Hermes compilation crashes on Android/iOS
+export default {
+  createClient: () => ({}),
+  createMicrophoneAudioTrack: () => ({}),
+};
