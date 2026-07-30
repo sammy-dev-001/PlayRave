02cae3a12a669a53fdd020db248c8495a7d1cf92
violetmoorway-9942 <violetmoorway@gmail.com>
2026-07-30 07:04:09 +0100

    fix: add missing GestureHandlerRootView wrapper to prevent native crashes on android

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useCallback } from 'react';
 import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as SplashScreen from 'expo-splash-screen';
 import SocketService from './src/services/socket';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -62,8 +63,10 @@ export default function App() {
   }, []);
 
   return (
-    <ThemeProvider>
-      <AppContent />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
